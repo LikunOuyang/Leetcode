f19faef1524cb93fed6772a8e4b08e8663bfff4f
LikunOuyang <34589418+LikunOuyang@users.noreply.github.com>
2020-12-01 15:15:53 -0500

    new problem

---
+++ Weekly Contest/1674_MinMovesToMakeArrayComplementary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	int minMoves(vector<int>& nums, int limit) {
+		int N = nums.size();
+		vector<int> moves_boundary(limit * 2 + 2, 0);
+		// collect moves boundary for all pairs
+		for (int i = 0; i < N / 2; ++i) {
+			int l = nums[i], r = nums[N - 1 - i];
+			--moves_boundary[min(l, r) + 1];
+			--moves_boundary[l + r];
+			++moves_boundary[l + r + 1];
+			++moves_boundary[max(l, r) + limit + 1];
+		}
+		int res = N, needed_moves = N;
+		// scan thru 2 to 2*Limit to find minimum moves needed
+		for (int i = 2; i <= limit * 2; ++i) {
+			needed_moves += moves[i];
+			res = min(res, needed_moves);
+		}
+		return ans;
+	}
+};
