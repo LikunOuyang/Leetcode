63c5a8346b14dcd75ed2beb5fca2d169f3d18d1d
LikunOuyang <34589418+LikunOuyang@users.noreply.github.com>
2020-12-01 22:10:55 -0500

    2020/11/28 Weekly Contest

--- Weekly Contest/1675_MinDeviationInArray.cpp
+++ Weekly Contest/1675_MinDeviationInArray.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
 	int minimumDeviation(vector<int>& nums) {
+		// optimized
+		priority_queue<int, vector<int>> max_heap;
+        int res = INT_MAX, min_n = INT_MAX;
+		for (auto n : nums) {
+            n = n % 2 ? n * 2 : n;
+			max_heap.push(n);
+            min_n = min(min_n, n);
+		}
+		while (!max_heap.empty() && max_heap.top() % 2 == 0) {
+            res = min(res, max_heap.top() - min_n);
+			min_n = min(min_n, max_heap.top() / 2);
+			max_heap.push(max_heap.top() / 2);
+            max_heap.pop();}
+		return min(res, max_heap.top() - min_n);
+		// original
+		/*
 		priority_queue <int, vector<int>, greater<int> > min_heap;
 		priority_queue <int, vector<int>> max_heap;
 		for (auto& it : nums) {
@@ -32,5 +48,6 @@ public:
 			res = std::min(res, max_heap.top() - min_heap.top());
 		}
 		return res;
+		*/
 	}
 };
